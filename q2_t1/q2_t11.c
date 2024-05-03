4a36283a7d136d3bf345cc32663a2cdffb35d5e8
elilfry <elifrydman08@gmail.com>
2024-05-03 09:49:18 +0300

    add q2 3.5

--- q2_t1/q2_t11.c
+++ q2_t1/q2_t11.c
@@ -17,8 +17,28 @@ long double k = strtold(argv[2], NULL);
 //calc poisson
 
 long double poisson = (exp(-lambda) * pow(lambda, k)) / tgamma(k + 1);
-    printf("Result: %.10Lf\n", poisson);
+    printf("Result: %.10Lf\n", poisson*100);
 
 
 return 0;
 }
+
+// cd q2_t1
+//gcc -o q2_t11  q2_t11.c -lm
+// ./q2_t11 5 10
+
+/*
+elifrydman@elifrydman-VirtualBox:~/Desktop/os_1/ex1_t_1_os/q2_t1$ ./q2_t11 5 10
+Result: 1.8132788708
+elifrydman@elifrydman-VirtualBox:~/Desktop/os_1/ex1_t_1_os/q2_t1$ ./q2_t11 2 1
+Result: 27.0670566473
+elifrydman@elifrydman-VirtualBox:~/Desktop/os_1/ex1_t_1_os/q2_t1$ ./q2_t11 2 10
+Result: 0.0038189851
+elifrydman@elifrydman-VirtualBox:~/Desktop/os_1/ex1_t_1_os/q2_t1$ ./q2_t11 2 2
+Result: 27.0670566473
+elifrydman@elifrydman-VirtualBox:~/Desktop/os_1/ex1_t_1_os/q2_t1$ ./q2_t11 3 3
+Result: 22.4041807655
+elifrydman@elifrydman-VirtualBox:~/Desktop/os_1/ex1_t_1_os/q2_t1$ ./q2_t11 100 3
+Result: 0.0000000000
+elifrydman@elifrydman-VirtualBox:~/Desktop/os_1/ex1_t_1_os/q2_t1$ 
+*/
