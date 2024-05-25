82232060b6743ef32edb1731d3557156857fbb43
elilfry <elifrydman08@gmail.com>
2024-05-26 02:28:24 +0300

    26.5 2:28 add q6

---
+++ q6_t1/add2PB.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+//how to run: ./add2PB <name> <middle_name> <last name>,<phone number>
+
+// for example:
+//./add2PB man mko,053-4535442
+int main(int argc, char *argv[])
+
+//max argc 5 min 3 
+{
+
+    if(argc < 2 || argc > 5) 
+    {
+        //print error
+        printf("error- please write again\n");
+        
+    }   
+
+    
+    FILE *file = fopen("phonebook.txt", "a");
+    if (file == NULL) {
+        perror("Failed to open the file");
+        exit(EXIT_FAILURE);
+    }
+
+    // for 1 name and phone number
+    if ((argc == 2) && (strchr(argv[1], ',') != NULL))
+    {
+        fprintf(file, "%s\n", argv[1]);
+        fclose(file);
+    }
+
+     // for 2 names and phone number
+    if (argc == 3)
+    {
+        fprintf(file, "%s %s\n", argv[1], argv[2]);
+        fclose(file);
+    }
+    
+    // for  3 names and phone number
+    if (argc == 4)
+    {
+            fprintf(file, "%s %s %s\n", argv[1], argv[2], argv[3]);
+            fclose(file);
+    }
+
+    // for 4 names and phone number
+    if (argc == 5)
+    {
+        fprintf(file, "%s %s %s %s\n", argv[1], argv[2], argv[3], argv[4]);
+        fclose(file);
+    }
+
+    
+    return 0;
+
+
+}
--- q6_t1/findPhone.c
+++ q6_t1/findPhone.c
@@ -1,48 +1,102 @@
+// function to find a phone number to a person from a text file called phonebpkk.txt:
+
+/*
+how to run the program
+- open terminal
+- type the following commands:
+    $ make
+    findPhone <name>
+
+*example: ./findPhone "John Doe"
+    output: 123-456-7890
+    if the name is not found, the output will be empty
+*/
+
+
+//./findPhone matan
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <fcntl.h>
 
+#define BUFFER_SIZE 256
 
-int main(int argc, char *argv[])
-
-
-{
-
-    if(argc < 3 || argc > 5) 
-    {
-        printf("error- please write again");
-    }   
-
-    
-    FILE *file = fopen("phonebook.txt", "a");
-    if (file == NULL) {
-        perror("Failed to open the file");
-        exit(EXIT_FAILURE);
+int main(int argc, char *argv[]) {
+    // Check if the correct number of arguments are provided
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <name>\n", argv[0]);
+        exit(1);
     }
 
-     //   1 name and phone number.
-    if (argc == 3)
-    {
-        fprintf(file, "%s,%s\n", argv[1], argv[2]);
-        fclose(file);
-    }
-    
-    // for  1 name,  1 second/last name and phone number
-    if (argc == 4)
-    {
-            fprintf(file, "%s %s,%s\n", argv[1], argv[2], argv[3]);
-            fclose(file);
+    // The name to search for
+    char *name = argv[1];
+
+    // Create a temporary file
+    char *temp_file = "temp.txt";
+
+    // Create the first child process
+    pid_t pid1 = fork();
+    if (pid1 < 0) {
+        perror("Fork failed");
+        exit(1);
+    } else if (pid1 == 0) {
+        // Open the temporary file
+        int fd = open(temp_file, O_WRONLY | O_CREAT, 0666);
+        if (fd < 0) {
+            perror("Open failed");
+            exit(1);
+        }
+
+        // Redirect stdout to the temporary file
+        dup2(fd, STDOUT_FILENO);
+        close(fd);
+
+        // Execute the grep command
+        char *grep_args[] = {"grep", name, "phonebook.txt", NULL};
+        execvp("grep", grep_args);
+
+        perror("execvp for grep failed");
+        exit(1);
     }
 
-    // for name, second name, last name and phone number
-    if (argc == 5)
-    {
-        fprintf(file, "%s %s %s,%s\n", argv[1], argv[2], argv[3], argv[4]);
-        fclose(file);
+    // Wait for the first child process to finish
+    waitpid(pid1, NULL, 0);
+
+    // Create the second child process
+    pid_t pid2 = fork();
+    if (pid2 < 0) {
+        perror("Fork failed");
+        exit(1);
+    } else if (pid2 == 0) {
+        // Open the temporary file
+        int fd = open(temp_file, O_RDONLY);
+        if (fd < 0) {
+            perror("Open failed");
+            exit(1);
+        }
+
+        // Redirect stdin from the temporary file
+        dup2(fd, STDIN_FILENO);
+        close(fd);
+
+        // Execute the cut command
+        char *cut_args[] = {"cut", "-d", ",", "-f", "2", NULL};
+        execvp("cut", cut_args);
+
+        perror("execvp for cut failed");
+        exit(1);
     }
 
-    
+    // Wait for the second child process to finish
+    waitpid(pid2, NULL, 0);
+
+    // Remove the temporary file
+    unlink(temp_file);
+
     return 0;
-
-
 }
+
+
